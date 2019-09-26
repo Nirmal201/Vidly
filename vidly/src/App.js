45b06af648cb894f0ea33df7b608d140bc66b8a8
Nirmal201 <ninu201@gmail.com>
2019-09-25 21:56:17 -0400

    Adding Routes and Creating Login Form

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rental from "./components/rental";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Movies></Movies>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/login" component={LoginForm} />
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rental" component={Rental} />
+          <Route path="/notfound" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/notfound" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
---
+++ vidly/src/components/loginForm.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Input from "./common/input";
+
+class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+    errors: {}
+  };
+  validate = () => {
+    return { username: "Username is required" };
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const errors = this.validate();
+    this.setState({ errors });
+
+    if (errors) return;
+
+    //call server
+    console.log("Submitted");
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+  render() {
+    const { account } = this.state;
+    return (
+      <div className="container">
+        <h1>LoginForm</h1>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+          ></Input>
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+          ></Input>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
