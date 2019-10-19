a1350e06a953b10f19dbadbc5919268d24423c2f
Nirmal201 <ninu201@gmail.com>
2019-10-19 18:55:55 -0400

    Form Refactoring

--- vidly/src/components/common/input.jsx
+++ vidly/src/components/common/input.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 
-const Input = ({ name, label, value, onChange, error }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        id={name}
-        name={name}
-        onChange={onChange}
-        value={value}
-        type="text"
-        className="form-control"
-      />
+      <input {...rest} id={name} name={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
